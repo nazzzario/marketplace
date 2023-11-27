a884c27fe0d9af72a2bccdd60f5d332d21512401
nazzzario <nazzzario23@gmail.com>
2023-11-27 20:52:40 +0200

    redeploy

--- src/main/java/com/teamchallenge/marketplace/password/dto/request/PasswordResetRequestDto.java
+++ src/main/java/com/teamchallenge/marketplace/password/dto/request/PasswordResetRequestDto.java
@@ -3,6 +3,6 @@ package com.teamchallenge.marketplace.password.dto.request;
 import jakarta.validation.constraints.Size;
 
 public record PasswordResetRequestDto(
-        @Size(min = 4, message = "Password min length is 4")
+        @Size(min = 8, message = "Password min length is 8")
         String newPassword) {
 }
