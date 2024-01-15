9bed1e30269f3e0edd3123f1ffe9137fe2777e37
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-01-15 12:52:38 +0200

    add methods for test
    Signed-off-by: Anton Sokolskyi <sokolplus3783@gmail.com>

---
+++ src/test/java/com/teamchallenge/marketplace/common/security/controller/AuthenticationControllerTest.java
@@ -0,0 +1,40 @@
+package com.teamchallenge.marketplace.common.security.controller;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class AuthenticationControllerTest {
+
+    @Test
+    void whenAuthenticateSuccessful() {
+    }
+    @Test
+    void whenAuthenticateFailed() {
+    }
+
+    @Test
+    void whenAuthenticateForbidden() {
+    }
+
+    @Test
+    void whenAuthenticateConflict() {
+
+    }
+
+    @Test
+    void whenAuthenticateByPhoneSuccessful() {
+    }
+    @Test
+    void whenAuthenticateByPhoneFailed() {
+    }
+
+    @Test
+    void whenAuthenticateByPhoneForbidden() {
+    }
+
+    @Test
+    void whenAuthenticateByPhoneConflict() {
+
+    }
+}
